b036eda4ef172a5b6412bedbfdca086dbe4d2f27
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-29 21:38:47 +0300

    Create close modal function

--- script.js
+++ script.js
@@ -9,8 +9,21 @@ openModalButtons.forEach((button) => {
   });
 });
 
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
 function openModal(modal) {
   if (modal == null) return;
   modal.classList.add("active");
   overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
